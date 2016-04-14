a6afb8fa9f84851386bf52adb7c2870a7282896f
fooying <fooying@qq.com>
2016-04-14 22:56:55 +0800

    添加DEMO

---
+++ dist/ifeng.desktopUI.js
@@ -0,0 +1,75 @@
+/*! Desktop UI React v0.0.1 | by ifeng IDC | (c) 2016 ifeng, Inc. | Licensed under MIT | 2016-04-14T22:53:19+0800 */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("react"));
+	else if(typeof define === 'function' && define.amd)
+		define(["react"], factory);
+	else if(typeof exports === 'object')
+		exports["DesktopUI"] = factory(require("react"));
+	else
+		root["DesktopUI"] = factory(root["React"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ }
+/******/ ])
+});
+;
---
+++ gulpfile.babel.js
@@ -0,0 +1,73 @@
+import path from 'path';
+import pkg from './package.json';
+import del from 'del';
+import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import gulp from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+
+import buildConfig from './webpack.build';
+
+const $ = loadPlugins();
+const paths = {
+  src: {
+    build: 'src/Desktop.js'
+  },
+  dist: {
+    build: './dist',
+    lib: './lib'
+  }
+};
+
+const buildDate = $.util.date(Date.now(), 'isoDateTime');
+const banner = [
+  '/*! <%= pkg.title %> v<%= pkg.version %>',
+  'by ifeng IDC',
+  '(c) ' + $.util.date(Date.now(), 'UTC:yyyy') + ' ifeng, Inc.',
+  'Licensed under <%= pkg.license %>',
+  buildDate + ' */\n'
+].join(' | ');
+
+gulp.task('build:pack', () => {
+  return gulp.src(paths.src.build)
+    .pipe(webpackStream(buildConfig))
+    .pipe($.replace('__VERSION__', pkg.version))
+    .pipe($.header(banner, {pkg: pkg}))
+    .pipe(gulp.dest(paths.dist.build))
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe($.header(banner, {pkg: pkg}))
+    .pipe(gulp.dest(paths.dist.build))
+    .pipe($.size({showFiles: true, title: 'minified'}))
+    .pipe($.size({showFiles: true, gzip: true, title: 'gzipped'}));
+});
+
+
+gulp.task('build:jsx', () => {
+  return gulp.src(['src/**/*.js', '!src/__tests__/*.js'])
+    .pipe($.if((file) => {
+      return file.path.indexOf('Desktop.js') > -1;
+    }, $.replace('__VERSION__', pkg.version)))
+    .pipe($.babel())
+    .pipe(gulp.dest(paths.dist.lib));
+});
+
+gulp.task('clean', () => {
+  return del([
+    paths.dist.lib,
+    paths.dist.build
+  ]);
+});
+
+gulp.task('build', (cb) => {
+  runSequence(
+    'clean',
+    ['build:pack', 'build:jsx'],
+    cb);
+});
+
+gulp.task('default', ['build:jsx']);
